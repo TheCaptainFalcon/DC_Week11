b1052df07f8e2aac7b4e69760e10a8d023e3181c
Joseph Kim <josephkim570@gmail.com>
2020-03-24 12:44:04 -0400

    Display single MemoryCard component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import MemoryCard from './Components/MemoryCard';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <h2 className="title">Memory Game</h2>
         <h2 className="subtitle">Match cards to win</h2>
       </header>
+      <MemoryCard />
     </div>
   );
 }
