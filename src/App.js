bc3592fb2d1ff15a52fee3c6c6d6e18fcde157ae
Joseph Kim <josephkim570@gmail.com>
2020-03-26 16:14:13 -0400

    finalized game logic - in working condition

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MemoryCard from './Components/MemoryCard';
 
 function generateDeck() {
   const symbols = [`∆`,` ß`, `£`, `§`,`•`, `$`, `+`, `ø`];
-  const deck = [];
+  let deck = [];
   for (let i = 0; i < 16; i++) {
     deck.push({
       symbol:symbols[i % 8],
@@ -32,34 +32,64 @@ class App extends Component {
     this.state = {
       deck: generateDeck(),
       pickedCards : []
-    }
-    function pickCard(cardIndex) {
-      if (this.state.deck[cardIndex].isFlipped) {
-        return
-        
+    };
+  }
+    pickCard = (cardIndex) => {
+      if (this.state.deck[cardIndex].isFlipped === true) {
+        return;
       } 
       let cardToFlip = {...this.state.deck[cardIndex]}
+      cardToFlip.isFlipped = true;
+      let newPickedCards = this.state.pickedCards.concat(cardIndex);
       let newDeck = this.state.deck.map((card, index) => {
         if (cardIndex === index) {
-          return cardToFlip
+          return cardToFlip;
         }
         return card;
-      })
+      });
+    if (newPickedCards.length === 2) {
+      let card1Index = newPickedCards[0];
+      let card2Index = newPickedCards[1];
+
+      if (newDeck[card1Index].symbol !== newDeck[card2Index].symbol) {
+        setTimeout(this.unflipCards.bind(this, card1Index, card2Index), 1000);
+      }
+      newPickedCards = [];
     }
     this.setState({
-      deck: newDeck, pickedCards: newPickedCards
-    })
+      deck: newDeck,
+      pickedCards: newPickedCards
+    });
   }
-   
+  
+  unflipCards(card1Index, card2Index) {
+    let card1 = {...this.state.deck[card1Index]};
+    let card2 = {...this.state.deck[card2Index]};
+    card1.isFlipped = false;
+    card2.isFlipped = false;
+    
+    let newDeck =this.state.deck.map((card, index) => {
+      if (index === card1Index) {
+        return card1;
+      }
+      if (index === card2Index) {
+        return card2;
+      }
+      return card;
+    });
+    this.setState({
+      deck: newDeck
+    });
+  }
+
   render() {  
-    let deck = [];    
+    let deck = [] 
     let cardsJSX = this.state.deck.map((card, index) => {
       return(
-        <MemoryCard symbol= {card.symbol} isFlipped= {card.isFlipped} key= {index}/>
+        <MemoryCard symbol= {card.symbol} isFlipped= {card.isFlipped} key= {index} pickCard= {this.pickCard.bind(this, index)}/>
       )
     })
-
-
+  
   return (
     <div className="App" >
       <header className="App-header" >
@@ -67,11 +97,20 @@ class App extends Component {
         <h2 className="subtitle">Match cards to win</h2>
       </header>
 
-      {cardsJSX.slice(0,4)}
-      {cardsJSX.slice(4,8)}
-      {cardsJSX.slice(8,12)}
-      {cardsJSX.slice(12,16)}
-      
+      <div>
+        <div>
+          {cardsJSX.slice(0,4)}
+        </div>
+        <div>
+          {cardsJSX.slice(4,8)}
+        </div>
+        <div>
+          {cardsJSX.slice(8,12)}
+        </div>
+        <div>
+          {cardsJSX.slice(12,16)}
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/MemoryCard.jsx
+++ src/Components/MemoryCard.jsx
@@ -2,22 +2,12 @@ import React, { Component } from 'react';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {    
-    constructor(props) {
-        super(props);
-        this.state = { isFlipped:false };
-    }
-
-    clickHandler() {
-        this.setState({
-            isFlipped: !this.state.isFlipped
-        }); 
-    }
-
+    
     render() {
         let memoryCardInnerClass = 'MemoryCardInner';
         this.props.isFlipped ? memoryCardInnerClass ='MemoryCardInner flipped' : memoryCardInnerClass = "MemoryCardInner";
         return (
-            <div className='MemoryCard' onClick={this.clickHandler.bind(this)}>
+            <div className='MemoryCard' onClick={this.props.pickCard}>
                 <div className={ memoryCardInnerClass }>
                     <div className='MemoryCardBack'>
                         <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt=""/>
